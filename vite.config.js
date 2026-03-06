f7b65c7c683f147a0cc54a75ae10b15e3c679c36
Subarna Tamang <subarnablone09@gmail.com>
2026-03-06 17:24:39 +0530

    changes done in config file

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,27 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),],
-    server: {
+  plugins: [react(), tailwindcss()],
+  server: {
     port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
+      }
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'router-vendor': ['react-router-dom'],
+          'redux-vendor': ['@reduxjs/toolkit', 'react-redux'],
+          'firebase-vendor': ['firebase/app', 'firebase/auth', 'firebase/analytics'],
+          'ui-vendor': ['swiper', 'lucide-react', 'radix-ui'],
+        }
       }
     }
   }
