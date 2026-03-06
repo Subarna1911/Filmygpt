8e8c71f0b96f471fe67c3448a82bdc2adf444334
Subarna Tamang <subarnablone09@gmail.com>
2026-03-06 17:40:24 +0530

    server side modfiication done

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
